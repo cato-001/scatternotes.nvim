1e35f96ee438764795405ec48ae129231b079dc7
Lukas412 <lukas.weber.4.12@gmail.com>
2024-02-11 21:09:49 +0100

    test lua nvim window

--- lua/scatternotes.lua
+++ lua/scatternotes.lua
@@ -3,8 +3,9 @@ local state = {}
 local function create_note()
 	local buffer = vim.api.nvim_create_buf(false, false)
 
-	local terminal_width = vim.o.winwidth
-	local terminal_height = vim.o.winheight
+	local current_window = vim.api.nvim_get_current_win()
+	local current_width = vim.api.nvim_win_get_width(current_window)
+	local current_height = vim.api.nvim_win_get_height(current_window)
 
 	local width = 40;
 	local height = 30;
@@ -12,8 +13,8 @@ local function create_note()
 		relative = 'editor',
 		width = width,
 		height = height,
-		col = (terminal_width / 2) - (width / 2),
-		row = (terminal_height / 2) - (height / 2),
+		col = (current_width / 2) - (width / 2),
+		row = (current_height / 2) - (height / 2),
 		anchor = 'NW',
 		style = 'minimal',
 	})
