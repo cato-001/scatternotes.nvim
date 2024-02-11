4c0370f4c4f10f63d623cfe559fb186fe5247f85
Lukas412 <lukas.weber.4.12@gmail.com>
2024-02-11 21:40:21 +0100

    fix type error

--- lua/scatternotes.lua
+++ lua/scatternotes.lua
@@ -7,8 +7,8 @@ local function create_note()
 	local current_width = vim.api.nvim_win_get_width(current_window)
 	local current_height = vim.api.nvim_win_get_height(current_window)
 
-	local width = (current_width / 10) * 8;
-	local height = (current_height / 10) * 6;
+	local width = math.floor(current_width * .8);
+	local height = math.floor(current_height * .6);
 	local window = vim.api.nvim_open_win(buffer, true, {
 		relative = 'editor',
 		width = width,
