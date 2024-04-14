00270d549e4c58b9dffcd904069556acc720dab9
Lukas412 <lukas.weber.4.12@gmail.com>
2024-04-15 00:58:26 +0200

    search feature prototype

--- lua/scatternotes/search.lua
+++ lua/scatternotes/search.lua
@@ -1,42 +1,57 @@
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
+local previewers = require('telescope.previewers')
 local config = require('telescope.config').values
 
 local function notes_search_picker(opts)
   opts = opts or {}
 
+  local finder = finders.new_job(
+    function(prompt)
+      if prompt == nil or prompt == "" then
+        return { "scatternotes", "list", "--show-tags" }
+      end
+
+      local command = { "scatternotes", "search" }
+      for tag in prompt:gmatch("%S+") do
+        table.insert(command, tag)
+      end
+      table.insert(command, "--show-tags")
+
+      return command
+    end,
+    function(entry)
+      local file = nil
+      local tags = ""
+
+      for item in entry:gmatch('[^|]+') do
+        if not file then
+          file = item
+        else
+          tags = item
+        end
+      end
+
+      return {
+        value = file,
+        display = tags,
+        ordinal = tags,
+        path = file
+      }
+    end
+  )
+  local sorter = config.generic_sorter(opts)
+  local previewer = previewers.new_termopen_previewer({
+    get_command = function(entry)
+      return { 'cat', entry.path }
+    end
+  })
+
   pickers.new(opts, {
     prompt_title = 'Note Tags',
-    finder = finders.new_job(
-      function(prompt)
-        if prompt == nil or prompt == "" then
-          return { "scatternotes", "list", "--show-tags" }
-        end
-
-        local command = { "scatternotes", "search" }
-        print(prompt)
-        for tag in prompt:gmatch("%S+") do
-          print(tag)
-          table.insert(command, tag)
-        end
-        table.insert(command, "--show-tags")
-        return command
-      end,
-      function(line)
-        local note = {}
-
-        for _, match in line:gmatch('[^|]+') do
-          table.insert(note, match)
-        end
-
-        return {
-          value = line,
-          display = note[2],
-          ordinal = note[2]
-        }
-      end
-    ),
-    sorter = config.generic_sorter(opts),
+    finder = finder,
+    sorter = sorter,
+    previewer = previewer
   }):find()
 end
 
