52c5d5eceb0c32789dc31659f24dcee8e806e5fe
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-25 16:36:27 +0200

    update setup

--- lua/scatternotes/setup.lua
+++ lua/scatternotes/setup.lua
@@ -3,10 +3,7 @@ local function commands(opts)
     return
   end
   local create_note = require('scatternotes.create').create_note
-  for _, value in ipairs(opts) do
-    local name = value[1]
-    local tags = value['tags']
-
+  for name, tags in pairs(opts) do
     vim.api.nvim_create_user_command(name, function() create_note(tags) end, {})
   end
 end
