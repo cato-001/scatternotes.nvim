52398947b7a58e29866fc82f63cddc2deeedfc9b
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-25 17:05:46 +0200

    expand setup function

--- lua/scatternotes/setup.lua
+++ lua/scatternotes/setup.lua
@@ -8,7 +8,22 @@ local function setup_commands(opts)
   end
 end
 
-return function(opts)
+local function setup_keymaps(opts)
+  if opts == nil then
+    return
+  end
+  local search_note = require('scatternotes.search').search_note
+  local search = opts['search']
+  if search ~= nil then
+    local name = table.remove(search, 1)
+    vim.keymap.set('n', name, search_note, search)
+  end
+end
+
+local function setup(opts)
   opts = opts or {}
   setup_commands(opts['commands'])
+  setup_keymaps(opts['keymaps'])
 end
+
+return setup
