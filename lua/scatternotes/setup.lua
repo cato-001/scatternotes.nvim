976fb52719bc0f30bbdd795497dc99d8eecf6e89
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-25 21:15:05 +0200

    add commit command to setup

--- lua/scatternotes/setup.lua
+++ lua/scatternotes/setup.lua
@@ -2,9 +2,17 @@ local function setup_commands(opts)
   if opts == nil then
     return
   end
-  local create_note = require('scatternotes.create').create_note
-  for name, tags in pairs(opts) do
-    vim.api.nvim_create_user_command(name, function() create_note(tags) end, {})
+  local create = opts['create']
+  if create ~= nil then
+    local create_note = require('scatternotes.create').create_note
+    for name, tags in pairs(create) do
+      vim.api.nvim_create_user_command(name, function() create_note(tags) end, {})
+    end
+  end
+  local commit = opts['commit']
+  if commit then
+    local commit_notes = require('scatternotes.commit').commit_notes
+    vim.api.nvim_create_user_command(commit, commit_notes, {})
   end
 end
 
